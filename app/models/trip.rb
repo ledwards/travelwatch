d87e0a008cd343392a1424c8b183e051febca05e
Lee Edwards <leeredwards@gmail.com>
2015-01-29 23:43:31 -0500

    Add description to trip API

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -24,11 +24,21 @@ class Trip
     flight_plans[midpoint].arrival_airport_code
   end
 
+  def description
+    desc = "#{items.first.start_at.strftime("%b %-d")} Trip"
+    desc += " to #{destination}" if destination
+    desc
+  end
+
   def start_at
     items.first.start_at
   end
 
   def end_at
     items.last.end_at
+  end
+
+  def as_json(options = {})
+    { flight_plans: flight_plans, hotel_bookings: hotel_bookings, car_rentals: car_rentals, description: description }
   end
 end
